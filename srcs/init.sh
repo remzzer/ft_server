a73078dbbc69bacf17bf1ecc23eab932745fb0b6
remi <hellokiziche@gmail.com>
2021-01-15 20:18:57 +0100

    config_wrdp

--- srcs/init.sh
+++ srcs/init.sh
@@ -18,17 +18,6 @@ ln -s /etc/nginx/sites-available/nginx.conf /etc/nginx/sites-enabled/
 apt install -y openssl
 openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout /etc/ssl/private/localhost.key -out /etc/ssl/certs/localhost.crt -subj "/C=FR/ST=Paris/L=Paris/O=42/OU=42/CN=localhost"
 
-#[INSTALL MARIADB]
-apt install -y mariadb-server
-service mysql start
-#Connect as Root user w/ no pwd & create database
-echo "CREATE DATABASE wordpress" | mysql -u root
-echo "GRANT ALL PRIVILEGES ON newdb.* TO 'root@localhost';" | mysql -u root
-echo "FLUSH PRIVILEGES;" | mysql -u root
-
-#[INSTALL WORDPRESS]
-
-
 #[SETTING PH]
 touch /var/www/localhost/info.php
 echo "<?php
@@ -38,6 +27,21 @@ echo "<?php
 #[INSTALL PHP]
 apt install -y php7.3-fpm php7.3-mysql
 service php7.3-fpm start
+
+#[INSTALL MARIADB]
+apt install -y mariadb-server
+service mysql start
+#Connect as Root user w/ no pwd & create database
+echo "CREATE DATABASE wordpress" | mysql -u root
+echo "GRANT ALL PRIVILEGES ON wordpress.* TO 'root@localhost';" | mysql -u root
+echo "FLUSH PRIVILEGES;" | mysql -u root
+
+#[INSTALL WORDPRESS]
+apt install -y wget
+cd tmp/
+wget -c https://wordpress.org/latest.tar.gz
+tar xzvf latest.tar.gz && rm latest.tar.gz
+mv wordpress/ /var/www/localhost/
 
 #[SETTING USER ACCESS RIGHT]
 chown -R www-data:www-data /var/www/* 
